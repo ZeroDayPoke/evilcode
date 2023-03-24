43e8f84f7cee839b34e423ae79c1d84abd5fe993
CStamper <cstampok@gmail.com>
2023-03-23 20:13:14 -0500

    adding zombie factory and fork demos

---
+++ shell/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUF_SIZE 150
+
+/**
+ * main - fork fun
+ * Return: Always 0
+ */
+int main(void)
+{
+	int pid = fork();
+	char buf[BUF_SIZE];
+	int print_count;
+
+	switch (pid)
+	{
+	case -1:
+		perror("fork failed");
+		exit(1);
+	case 0:
+		/* When fork() returns 0, we are in the child process. */
+		print_count = 10;
+		sprintf(buf, "child process: pid = %d", pid);
+		break;
+    default: /* + */
+		/* When fork() returns a positive number, we are in the parent process */
+		/* (the fork return value is the PID of the newly created child process) */
+		print_count = 5;
+		sprintf(buf, "parent process: pid = %d", pid);
+		break;
+	}
+	for (; print_count > 0; print_count--)
+	{
+		puts(buf);
+		sleep(1);
+	}
+	exit(0);
+}
